05b082d620281dc13f8d9eab761c8232148f6576
Dalton-K-Hughes <dhughes3223@gmail.com>
2024-08-17 17:08:39 -0400

    Added base init stuff for the app

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,23 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    config_type = os.getenv('FLASK_CONFIG', 'development')
+    
+    if config_type == 'production':
+        app.config.from_object('website.config.ProductionConfig')
+    elif config_type == 'testing':
+        app.config.from_object('website.config.TestingConfig')
+    else:
+        app.config.from_object('website.config.DevelopmentConfig')
+        
+    db.init_app(app)
+    
+    
     
     from .views import views
     
